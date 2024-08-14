4ec9d220efa0750ca7a23e1cf38db1e360d76a61
shreygupta799 <shreygupta799@gmail.com>
2024-08-14 18:41:26 +0530

    removed provider

--- lib/view/device_info_view.dart
+++ lib/view/device_info_view.dart
@@ -1,44 +1,74 @@
+// views/device_info_screen.dart
+
 import 'package:flutter/material.dart';
+import 'package:nudge_test/model/device_info_model.dart';
 import 'package:nudge_test/view_model/device_info_view_model.dart';
-import 'package:provider/provider.dart';
 
+class DeviceInfoScreen extends StatefulWidget {
+  @override
+  _DeviceInfoScreenState createState() => _DeviceInfoScreenState();
+}
 
-class DeviceInfoScreen extends StatelessWidget {
+class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
+  final DeviceInfoViewModel _viewModel = DeviceInfoViewModel();
+
+  @override
+  void initState() {
+    super.initState();
+    _viewModel.loadDeviceInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => DeviceInfoViewModel()..loadDeviceInfo(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Device Information'),
-        ),
-        body: Consumer<DeviceInfoViewModel>(
-          builder: (context, viewModel, child) {
-            final deviceInfoModel = viewModel.deviceInfoModel;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Device Information"),
+      ),
+      body: FutureBuilder<void>(
+        future: _viewModel.loadDeviceInfo(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            return _buildDeviceInfo(_viewModel.deviceInfoModel);
+          }
+        },
+      ),
+    );
+  }
 
-            if (deviceInfoModel == null) {
-              return Center(child: CircularProgressIndicator());
-            }
+  Widget _buildDeviceInfo(DeviceInfoModel? deviceInfo) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildInfoRow("Timezone", deviceInfo?.timezone),
+          _buildInfoRow("Internet State", deviceInfo?.internetState.toString()),
+          _buildInfoRow("Brand", deviceInfo?.brand),
+          _buildInfoRow("Model", deviceInfo?.model),
+          _buildInfoRow("Device Language", deviceInfo?.deviceLanguage),
+          _buildInfoRow("App Version", deviceInfo?.appVersion),
+          _buildInfoRow("Battery Level", deviceInfo?.batteryLevel.toString()),
+          _buildInfoRow("OS", deviceInfo?.os),
+          _buildInfoRow("OS Version", deviceInfo?.osVersion),
+          _buildInfoRow("Build Mode", deviceInfo?.buildMode),
+          _buildInfoRow("Height", deviceInfo?.height.toString()),
+          _buildInfoRow("Width", deviceInfo?.width.toString()),
+        ],
+      ),
+    );
+  }
 
-            return ListView(
-              padding: EdgeInsets.all(16.0),
-              children: [
-                Text('Timezone: ${deviceInfoModel.timezone ?? 'N/A'}'),
-                Text('Internet State: ${deviceInfoModel.internetState ? 'Connected' : 'Disconnected'}'),
-                Text('Brand: ${deviceInfoModel.brand}'),
-                Text('Model: ${deviceInfoModel.model}'),
-                Text('Device Language: ${deviceInfoModel.deviceLanguage}'),
-                Text('App Version: ${deviceInfoModel.appVersion}'),
-                Text('Battery Level: ${deviceInfoModel.batteryLevel}'),
-                Text('OS: ${deviceInfoModel.os}'),
-                Text('OS Version: ${deviceInfoModel.osVersion}'),
-                Text('Build Mode: ${deviceInfoModel.buildMode}'),
-                Text('Height: ${deviceInfoModel.height} pixels'),
-                Text('Width: ${deviceInfoModel.width} pixels'),
-              ],
-            );
-          },
-        ),
+  Widget _buildInfoRow(String label, String? value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(label, style: TextStyle(fontWeight: FontWeight.bold)),
+          Text(value ?? "Unknown"),
+        ],
       ),
     );
   }
